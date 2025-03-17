1d95dbaf1bb55e2187a8dab8fe02a0e7db1206c1
Doyler <37679344+vemork@users.noreply.github.com>
2025-03-17 00:19:55 -0500

    Add files via upload
    payments service

---
+++ payments/services/channel-service.js
@@ -0,0 +1,18 @@
+
+
+/**
+ * Service class for handling payments business logic.
+ * @class ChannelService
+ */
+class ChannelService {
+    /**
+     * Publishes a payment notification.
+     * @returns {Promise<void>}
+     */
+
+    async publishPaymentNotification(payment) {
+        console.log('Payment notification published');
+    }
+}
+
+export default ChannelService;
---
+++ payments/services/payment-service.js
@@ -0,0 +1,59 @@
+import {createPaymentSessionDto} from "#features/payments/dto/payment-session-dto.js";
+
+/**
+ * Service class for handling payments business logic.
+ * @class PaymentService
+ */
+class PaymentService {
+    /** @private */
+    #channelService;
+    #stripeService;
+    /**
+     * inject instance of ChannelService.
+     *
+     * @param {Object} channelService
+     * @param stripeService
+     */
+    constructor(channelService, stripeService) {
+        /**
+         * @private
+         * @type {Object}
+         */
+        this.#channelService = channelService;
+        this.#stripeService = stripeService;
+    }
+    /**
+     * Publishes a payment notification.
+     * @param {number} id - The payment identifier.
+     * @returns {Promise<void>}
+     */
+    async publishPaymentNotification(id) {
+        return this.#channelService.publishPaymentNotification(id);
+    }
+
+    async createPaymentSession(payment) {
+        const dto = createPaymentSessionDto(payment);
+        const session = await this.#stripeService.checkout.sessions.create({
+            payment_intent_data: {
+                metadata: {}
+            },
+            line_items: [{price_data: {currency: dto.currency, product_data: {name: 'Ticket'}, unit_amount: dto.amount}, quantity: dto.tickets.length}],
+            mode: 'payment',
+            success_url: 'http://localhost:5000/api/payments/success', // llevar a una var de env
+            cancel_url: 'http://localhost:5000/api/payments/cancel',
+        });
+        console.log("session", session);
+        return {
+            status: 'success',
+            sessionId: session.id,
+            info: session
+        };
+    }
+
+    async webhookService() {
+        return "webhook successful called.";
+    }
+    
+}
+
+export default PaymentService;
