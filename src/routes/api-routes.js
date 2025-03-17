9cb08984630d91e3af226e6393329bdfeb5eddd6
Devisson Vasquez <devissonvasquez99@gmail.com>
2025-03-17 12:39:05 -0500

    feat: implement winners feature with repository, controller, routes, and validations

--- src/routes/api-routes.js
+++ src/routes/api-routes.js
@@ -4,7 +4,6 @@ import userRoutes from '#features/users/api/user-routes.js';
 import raffleRoutes from '#features/raffles/api/raffle-routes.js';
 import countryRoutes from '#features/countries/api/country-routes.js';
 import ticketRoutes from '#features/tickets/api/ticket-routes.js';
-import paymentRoutes from '#features/payments/api/payment-routes.js';
 import winnerRoutes from '#features/winners/api/winner-routes.js';
 import paymentRoutes from '#features/payments/api/payment-routes.js';
 
@@ -15,7 +14,6 @@ apiRoutes.use('/health-checks', healthCheckRoutes);
 apiRoutes.use('/raffles', raffleRoutes);
 apiRoutes.use('/countries', countryRoutes);
 apiRoutes.use('/tickets', ticketRoutes);
-apiRoutes.use('/payments', paymentRoutes);
 apiRoutes.use('/winners', winnerRoutes);
 apiRoutes.use('/payments', paymentRoutes);
 
