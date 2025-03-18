1ee8b980bfd22b0a0af0b34a01984ccf868744f8
Devisson Vasquez <devissonvasquez99@gmail.com>
2025-03-17 22:17:17 -0500

    chore: update package.json with eslint-plugin-prettier and clean up code formatting in payment and winner modules

--- src/features/payments/services/channel-service.js
+++ src/features/payments/services/channel-service.js
@@ -1,18 +1,16 @@
-
-
 /**
  * Service class for handling payments business logic.
  * @class ChannelService
  */
 class ChannelService {
-    /**
-     * Publishes a payment notification.
-     * @returns {Promise<void>}
-     */
+  /**
+   * Publishes a payment notification.
+   * @returns {Promise<void>}
+   */
 
-    async publishPaymentNotification(payment) {
-        console.log('Payment notification published');
-    }
+  async publishPaymentNotification(payment) {
+    console.log('Payment notification published');
+  }
 }
 
 export default ChannelService;
