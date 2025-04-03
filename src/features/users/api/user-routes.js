20c1faf99cc1da121e8e3e9710bb0ee2a1c62bee
Devisson Vasquez <devissonvasquez99@gmail.com>
2025-04-02 23:09:50 -0500

    feat: update user routes to allow access for both admin and user roles

--- src/features/users/api/user-routes.js
+++ src/features/users/api/user-routes.js
@@ -8,20 +8,20 @@ const router = Router();
 router.get(
   '/',
   authenticate,
-  authorize([envs.ROLE_ADMIN]),
+  authorize([envs.ROLE_ADMIN, envs.ROLE_USER]),
   userController.getAll,
 );
 router.get(
   '/:id',
   authenticate,
-  authorize([envs.ROLE_ADMIN]),
+  authorize([envs.ROLE_ADMIN, envs.ROLE_USER]),
   userController.getById,
 );
 router.post('/', userController.create);
 router.patch(
   '/:id',
   authenticate,
-  authorize([envs.ROLE_ADMIN]),
+  authorize([envs.ROLE_ADMIN, envs.ROLE_USER]),
   userController.update,
 );
 router.delete(
