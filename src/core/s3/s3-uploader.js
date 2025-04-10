acfd3f47e01fc7d26a83ec9538e0546ddd41d3d6
Devisson Vasquez <devissonvasquez99@gmail.com>
2025-04-09 20:14:00 -0500

    feat: add uploadPublicFile function to handle public file uploads to S3

--- src/core/s3/s3-uploader.js
+++ src/core/s3/s3-uploader.js
@@ -3,6 +3,15 @@ import { PutObjectCommand } from '@aws-sdk/client-s3';
 import { v4 as uuid } from 'uuid';
 import { envs } from '#core/config/envs.js';
 
+/**
+ * Uploads a public file to an S3 bucket.
+ * @param {Buffer} fileBuffer - The buffer containing the file data to be uploaded.
+ * @param {string} fileName - The name of the file, including its extension.
+ * @param {string} folder - The folder path within the S3 bucket where the file will be stored.
+ * @returns {Promise<string>} - A promise that resolves to the public URL of the uploaded file.
+ *
+ * @throws {Error} - Throws an error if the upload process fails.
+ */
 export const uploadPublicFile = async (fileBuffer, fileName, folder) => {
   const extension = fileName.split('.').pop();
   const key = `${folder}/${uuid()}.${extension}`;
